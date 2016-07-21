e4c1ac70eaa56aada962a02916b205655172695e
Zach Johnson <zachary.willis.johnson@gmail.com>
2016-07-21 13:55:55 -0500

    All test cases passed

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
 #write your code here
+
+def ftoc(n)
+	return ((n-32)/1.8).round
+	#test case passing requires whole numbers
+end
+
+def ctof(n)
+	return ((n*1.8)+32)
+	#test case passing does not require whole numbers
+end
