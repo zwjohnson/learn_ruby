e958de15d9439a82aba0d2fff1467aa80439d066
Zach Johnson <zachary.willis.johnson@gmail.com>
2016-07-23 13:06:52 -0500

    All tests passed

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
-# write your code here
+
+def title=(title)
+	words = title.split.map do |word|
+		if %w(and or the a in of an).include?(word)
+			word
+		else
+			word.capitalize
+		  end
+		end
+	words.first.capitalize!
+	return @title = words.join(" ")
+
+	end
+	def title
+	   @title
+	end
+	
 end
+
+	
