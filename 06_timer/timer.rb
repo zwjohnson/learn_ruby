5e32f299fa236d0dc4d15266b3bf9a679a193688
Zach Johnson <zachary.willis.johnson@gmail.com>
2016-07-24 13:35:23 -0500

    All tests passed

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,37 @@
 class Timer
-  #write your code here
+
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds= seconds
+    @seconds = seconds
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def time_string
+    # Get the timer's current seconds instance variable
+    seconds = @seconds
+
+    # Calculate hours
+    hours = seconds / 3600
+    seconds -= hours * 3600
+
+    # Calculate minutes
+    minutes = seconds / 60
+    seconds -= minutes * 60
+
+    # Store the time values in a string array
+    time_string = [
+      hours.to_s.rjust(2, '0'),
+      minutes.to_s.rjust(2, '0'),
+      seconds.to_s.rjust(2, '0')
+    ]
+
+    # Return the time string
+    time_string.join(':')
+  end
 end
